eefa6773bd640fb1025d4d11872bde1bf657e66c
bsu-leschik <pimako2003@yandex.by>
2022-03-29 20:48:52 +0300

    Made second task

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -3,24 +3,31 @@
 #include <bitset>
 
 extern "C" double first_task(double, double, double);
+extern "C" double second_task(int);
+extern "C" double third_task(int);
+extern "C" double fifth_task(int);
 
 double task1(double a, double b, double d);
-double task2();
-double task3();
-double task4();
+double task2(int precesion);
+double task3(int precesion);
+double task4(int precesion);
 
 int main()
 {
 
     double a = 1, b = 1, c = -24.375;
+    int precesion = 5;
 
     std::cout << "Enter a,b,c for first task: ";
     std::cin >> a >> b >> c;
     std::cout << std::endl;
-    std::cout << "Task1: " << std::fixed << std::setprecision(6) << task1(a, b, c) << std::endl <<
-        "Task2: " << task2() << std::endl <<
-        "Task3: " << task3() << std::endl <<
-        "Task4: " << task4() << std::endl;
+    std::cout << "Enter precison for second and third tasks: ";
+    std::cin >> precesion;
+    std::cout << std::endl;
+    std::cout << "Task1: " << std::fixed << std::setprecision(10) << task1(a, b, c) << std::endl <<
+        "Task2: " << task2(precesion) << std::endl <<
+        "Task3: " << task3(precesion) << std::endl <<
+        "Task4: " << task4(precesion) << std::endl;
 
 }
 
@@ -31,14 +38,16 @@ double task1(double a, double b, double d) {
     return answer;
 }
 
-double task2() {
+double task2(int precesion) {
+    double answer;
+    answer = second_task(precesion);
+    return answer;
+}
+
+double task3(int precesion) {
     return 0;
 }
 
-double task3() {
-    return 0;
-}
-
-double task4() {
+double task4(int precesion) {
     return 0;
 }
---
+++ Lab4/second.asm
@@ -0,0 +1,43 @@
+.model flat, C 
+.const
+	twelwe real4 12.0;
+.data
+	temp real4 0.0;
+.code
+	second_task PROC, precesion: DWORD
+		mov ECX, precesion;
+		FLDZ;{sum}
+		FLD1;{1, sum}
+		FLD1;{1, k, sum}
+		FLD1;{k, 1, k, sum}
+
+	_start:
+		FMUL st(0), st(0);{k^2, (-1)^(k + 1), k, sum}
+		FMUL st(0), st(1);{(-1)^(k + 1)*k^2, (-1)^(k + 1), k, sum}
+		FLD1;{1, (-1)^(k + 1)*k^2, (-1)^(k + 1), k, sum}
+		FDIV st(0), st(1);{1/((-1)^(k + 1)*k^2), k^2, (-1)^(k + 1), k, sum}
+		FST temp;
+
+		FADDP st(4), st(0);{(-1)^(k + 1)*k^2, (-1)^(k + 1), k, sum}
+
+		FADDP st(0), st(0);{(-1)^(k + 1), k, sum}
+		FCHS;{(-1)^(k + 2), k, sum}
+
+		FXCH st(1);{k, (-1)^(k + 2), sum}
+		FLD1;{1, k, (-1)^(k + 2), sum}
+		FADDP st(1), st(0);{k + 1, (-1)^(k + 2), sum}
+		FXCH st(1);{(-1)^(k + 2), k + 1, sum}}
+
+		FLDZ;{0, (-1)^(k + 2), k, sum}
+		FADD st(0), st(2);{k, (-1)^(k + 2), k, sum}
+
+		loopd _start;
+
+		FADDP st(0), st(0);
+		FADDP st(0), st(0);
+		FADDP st(0), st(0);
+		FMUL twelwe;
+		FSQRT;
+		ret;
+	second_task ENDP
+end
